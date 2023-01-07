50ff606996561ea96f23c9b4978e710f862408ec
Hinalpatel8988 <118151523+Hinalpatel8988@users.noreply.github.com>
2023-01-07 19:19:17 +1100

    questions and answers added to the script.js

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+const question = [
+    {
+        question: "Questions 1 : Javascript is an _______ language?",
+        choices: ["a. Object-Oriented", "b. Object-Based", "c. Procedural", "d. None of the above"],
+        answer: "a"
+    },
+    {
+        question: "Questions 2 : Which of the following methods is used to access HTML elements using Javascript?",
+        choices: ["a. getElementbyid()", "b. getElementByClassName()", "c. Both A and B", "d. None of the above"],
+        answer: "c"
+    },
+    {
+        question: "Questions 3 : How do you create a function in JavaScript",
+        choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
+        answer: "b"
+    },
+    {
+        question: "Questions 4 : How can a datatype be declared to be a constant type?",
+        choices: ["a. const", "b. var", "c. let", "d. constant"],
+        answer: "a"
+    },
+    {
+        question: "Questions 5 : What keyword is used to check whether a given property is valid or not?",
+        choices: ["a. in", "b. as in", "c. exists", "d. lies"],
+        answer: "a"
+    },
+    {
+        question: "Questions 6 : The first index of an array is ____.",
+        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
+        answer: "a"
+    },
+    {
+        question: "Questions 7 : Which of the following keywords is used to define a variable in Javascript?",
+        choices: ["a. var", "b. let", "c. Both A and B", "d. None of the above"],
+        answer: "c"
+    };
+];
